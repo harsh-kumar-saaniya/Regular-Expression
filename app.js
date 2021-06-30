d9c206232f025a0f2bb784a879246073fb16130e
harsh-kumar-saaniya <harshvithal1@gmail.com>
2021-06-30 15:55:34 -0700

    Learn Second function test func in regex

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ let a = "This is the programmer guy harish vithal";
 
 let result = reg.exec(a)
 console.log(result);
+
+// FUNCTION 2 test ===> RETURNS TRUE OR FALSE
+let result2 = reg.test("harish");
+console.log(result2); 
