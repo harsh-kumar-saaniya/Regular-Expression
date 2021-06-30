171e372462cbca09b4ab6751e6871039ac748ab6
harsh-kumar-saaniya <harshvithal1@gmail.com>
2021-06-30 16:01:48 -0700

    Learn third match function in regex

--- app.js
+++ app.js
@@ -9,4 +9,11 @@ console.log(result);
 
 // FUNCTION 2 test ===> RETURNS TRUE OR FALSE
 let result2 = reg.test("harish");
-console.log(result2); 
+console.log(result2);
+
+// FUNCTION 3 MATCH ===> IT WILL RETURN AN ARRAY OR NULL
+
+// let result3 = reg.match(a) ===> this is wrong syntax
+// let result3 = a.match(reg) ===> this is right syntax
+let result3 = a.match(reg)
+console.log(result3);
